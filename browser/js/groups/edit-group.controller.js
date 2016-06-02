df6a17ce917caaa02ccf00a8fd635fa4ca69e9ba
Eric Traub <est2114@columbia.edu>
2016-06-02 17:37:38 -0400

    messages for add member functional

--- browser/js/groups/edit-group.controller.js
+++ browser/js/groups/edit-group.controller.js
@@ -14,19 +14,29 @@ app.controller('EditGroupCtrl', function($scope, GroupFactory, group) {
 		GroupFactory.addMemberToGroup($scope.searchedEmail, $scope.group._id)
 		.then(function(message) {
 			$scope.searchedEmail = '';
-			console.log(message)
 			if (message === 'member already in group') {
 				$scope.userNotFound = false;
-				$scope.memberAlreadyPresent = true;
+				$scope.memberAdded = false;	
+				$scope.memberAlreadyPresent = true;	
+				setTimeout(function() {
+					$('#alt2').fadeOut();
+				}, 6000)
 			}
 			else if(message === 'user not found') {
 				$scope.memberAlreadyPresent = false;
 				$scope.userNotFound = true;
+				setTimeout(function() {
+					$('#alt1').fadeOut();
+				},15000)
 			}
 			else if (message === 'member added') {
 				$scope.memberAlreadyPresent = false;
 				$scope.userNotFound = false;
+				$scope.memberAdded = false;
 				$scope.memberAdded = true;
+				setTimeout(function() {
+					$('#alt3').fadeOut();
+				}, 6000)
 			}
 		});
 	};
