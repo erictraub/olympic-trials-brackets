2b9622ccac5a7b526ddef76cdc28dddd6882c2a5
Eric Traub <est2114@columbia.edu>
2016-06-05 15:03:06 -0400

    updates

--- server/env/development.js
+++ server/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/trials-bracket",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "SESSION_SECRET": "Optimus Prime is my real",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
     "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
