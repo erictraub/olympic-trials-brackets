81e2013e00daaed137b3df65caeb0e1fe687f82e
Eric Traub <est2114@columbia.edu>
2016-07-05 18:34:22 -0400

    admin updated

--- browser/js/admin/admin.state.js
+++ browser/js/admin/admin.state.js
@@ -3,5 +3,13 @@ app.config(function ($stateProvider) {
         url: '/admin',
         templateUrl: 'js/admin/admin.template.html',
         controller: 'AdminCtrl',
+        resolve: {
+        	user: function(AuthService, $state) {
+        		AuthService.getLoggedInUser()
+        		.then(function(user) {
+        			if (!user || !user.isAdmin) $state.go('login');
+        		});
+        	}
+        }
     });
 });
